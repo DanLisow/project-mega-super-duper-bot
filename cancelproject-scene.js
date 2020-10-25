9f4a4064bd77d6fe8c2fa23f63074b0c68994566
DanLisow <56221679+DanLisow@users.noreply.github.com>
2020-10-25 22:02:11 +0500

    Add files via upload

---
+++ cancelproject-scene.js
@@ -0,0 +1,63 @@
+const { Telegraf } = require("telegraf");
+const { Markup } = Telegraf;
+const Composer = require("telegraf/composer");
+const Stage = require("telegraf/stage");
+const WizardScene = require("telegraf/scenes/wizard");
+const mysql = require("mysql");
+const { leave } = Stage;
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "T9dudEP9AJhX79i",
+    database: "practice",
+});
+
+connection.query("SELECT * FROM `schedule`", (err, res) => console.log(err));
+connection.query("SET SESSION wait_timeout = 604800");
+
+function cancelprojectSceneGenerate() {
+    const stepHandler = new Composer();
+
+    const cancelproject = new WizardScene(
+        "cancelproject",
+        (ctx) => {
+            ctx.reply("1. Напишите название команды: ");
+            ctx.wizard.next();
+        },
+        (ctx) => {
+            ctx.wizard.state.team = ctx.message.text;
+            connection.query(`SELECT * FROM schedule WHERE team = '${ctx.wizard.state.team.toLowerCase()}'`, (err, res) => {
+                if (res[0] == undefined) {
+                    ctx.replyWithHTML("❗️ Вашей команды нет в списке\n\n🔹<b>Выбери, что нужно:</b>");
+                    ctx.scene.leave();
+                }
+                else {
+                    ctx.reply("Вы хотите удалить запись ? (Да/Нет)");
+                    ctx.wizard.next();
+                }
+            });
+        },
+        (ctx) => {
+            if (ctx.message.text.toLowerCase() == "да") {
+                connection.query(`DELETE FROM schedule WHERE team = '${ctx.wizard.state.team}'`, (err, res) => {
+                    ctx.replyWithHTML("Ваша запись удалена...\n\n🔹<b>Выбери, что нужно:</b>");
+                });
+            }
+            else if (ctx.message.text.toLowerCase() == "нет") {
+                ctx.replyWithHTML("Так зачем нажал тогда?...\nЯ ухожу...\n\n🔹<b>Выбери, что нужно:</b>");
+            }
+            else {
+                ctx.replyWithPhoto({ source: 'кот.jpg' });
+                // ctx.replyWithHTML("Запись не удалена...\n\n🔹<b>Выбери, что нужно:</b>")
+            }
+            ctx.scene.leave();
+        }
+    );
+
+    return cancelproject;
+}
+
+
+
+module.exports = cancelprojectSceneGenerate;
---
+++ checkproject-scene.js
@@ -0,0 +1,48 @@
+const { Telegraf } = require("telegraf");
+const { Markup } = Telegraf;
+const Composer = require("telegraf/composer");
+const Stage = require("telegraf/stage");
+const WizardScene = require("telegraf/scenes/wizard");
+const mysql = require("mysql");
+const { leave } = Stage;
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "T9dudEP9AJhX79i",
+    database: "practice",
+});
+
+connection.query("SELECT * FROM `schedule`", (err, res) => console.log(err));
+connection.query("SET SESSION wait_timeout = 604800");
+
+function checkprojectSceneGenerate() {
+    const stepHandler = new Composer();
+
+    const checkproject = new WizardScene(
+        "checkproject",
+        (ctx) => {
+            ctx.reply("1. Напишите название команды: ");
+            ctx.wizard.next();
+        },
+        (ctx) => {
+            ctx.wizard.state.team = ctx.message.text;
+            connection.query(`SELECT * FROM schedule WHERE team = '${ctx.wizard.state.team.toLowerCase()}'`, (err, res) => {
+                if (res[0] == undefined) {
+                    ctx.replyWithHTML("❗️ Вашей команды нет в списке\n\n🔹<b>Выбери, что нужно:</b>");
+                    ctx.scene.leave();
+                }
+                else {
+                    ctx.replyWithHTML(`Ваше время: <b>${res[0].time}</b>\nВаше направление: <b>${res[0].data}</b>\n\n🔹<b>Выбери, что нужно:</b>`);
+                    ctx.scene.leave();
+                }
+            });
+        }
+    );
+
+    return checkproject;
+}
+
+
+
+module.exports = checkprojectSceneGenerate;
